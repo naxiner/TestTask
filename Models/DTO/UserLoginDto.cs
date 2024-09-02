17bc905bdc7fc7ffb737ce2a8aa4b4736658101d
naxiner <kirienkodenis2002@gmail.com>
2024-09-02 15:54:36 +0300

    Add DTO User Models

---
+++ Models/DTO/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TestTask.Models.DTO
+{
+    public class UserLoginDto
+    {
+        public string UsernameOrEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/DTO/UserRegistrationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TestTask.Models.DTO
+{
+    public class UserRegistrationDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
