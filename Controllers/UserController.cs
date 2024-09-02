4789f2b7b1115e453f71fb6556622a77523acf0d
naxiner <kirienkodenis2002@gmail.com>
2024-09-02 16:35:17 +0300

    Add Register for User Controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TestTask.Data;
+using TestTask.Models;
 using TestTask.Models.DTO;
+using TestTask.Repositories;
 
 namespace TestTask.Controllers
 {
@@ -8,17 +10,36 @@ namespace TestTask.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IUserRepository _userRepository;
 
-        public UserController(ApplicationDbContext context)
+        public UserController(IUserRepository userRepository)
         {
-            _context = context;
+            _userRepository = userRepository;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserRegistrationDto userDto)
         {
-            return Ok();
+            if (_userRepository.GetByNameAsync(userDto.Username) != null || 
+                _userRepository.GetByEmailAsync(userDto.Email) != null)
+            {
+                return BadRequest("Username or Email already exists.");
+            }
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
+
+            var user = new User
+            {
+                Username = userDto.Username,
+                Email = userDto.Email,
+                PasswordHash = hashedPassword,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            await _userRepository.Add(user);
+
+            return Ok("User registered successfully.");
         }
 
         [HttpPost("login")]
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,6 +6,6 @@ namespace TestTask.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
