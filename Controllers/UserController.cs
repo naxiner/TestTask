754749eeebb47765296206d38f7f3ffe1b50190f
naxiner <kirienkodenis2002@gmail.com>
2024-09-02 15:59:05 +0300

    Add UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestTask.Data;
+using TestTask.Models.DTO;
+
+namespace TestTask.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegistrationDto userDto)
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDto loginDto)
+        {
+            return Ok();
+        }
+    }
+}
