4e6335f15a42fe209ca0fe74d3cfb0600f128616
naxiner <kirienkodenis2002@gmail.com>
2024-09-02 16:34:49 +0300

    Add UserRepository

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using TestTask.Models;
+
+namespace TestTask.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(Guid userId);
+        Task<User> GetByNameAsync(string username);
+        Task<User> GetByEmailAsync(string email);
+        Task<bool> Add(User user);
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+
+namespace TestTask.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByIdAsync(Guid userId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public async Task<User> GetByNameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<bool> Add(User user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
